d345d048c19ad147448b397d0e9946a33072e75e
vinay <vinaychandola@gmail.com>
2023-12-11 01:23:46 +0530

    update: initial files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { useState } from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/Header/Header';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import AllApplicantsPage from "./pages/AllApplicantsPage/AllApplicantsPage";
+import PostJobPage from "./pages/PostJobPage/PostJobPage";
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('dashboard');
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+       <Route path="/" element={<DashboardPage />} />
+       <Route path="/post" element={<PostJobPage />} />
+       <Route path="/all" element={<AllApplicantsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/AllApplicantsPage/AllApplicantsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllApplicantsPage = () => {
+  return (
+    <div>AllApplicantsPage</div>
+  )
+}
+
+export default AllApplicantsPage
